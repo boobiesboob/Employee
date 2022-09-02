4564a9460b435f9fe38047c13018fe9caee8fbe2
Mo$he <boobiesboob@gmx.net>
2022-09-02 18:21:53 +0200

    Jenja, ich habe diese zwei neue Dateien hinzugefügt

---
+++ src/Office.java
@@ -0,0 +1,9 @@
+public class Office {
+
+    private Employee employee;
+
+    public Office(){
+        employee = new Employee("roma", "kap", 100);
+    }
+
+}
---
+++ src/Work.java
@@ -0,0 +1,8 @@
+public class Work {
+
+    private Employee employee;
+
+    public Work(){
+        employee = new Employee("roma", "kap", 100);
+    }
+}
